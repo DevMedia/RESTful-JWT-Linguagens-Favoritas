cbd2e4b7510a854f289338a467d4b71afd6b76bd
CaioRolla <caiioocesar@outlook.com>
2018-12-10 17:28:23 -0200

    ENVs! ENVs everywhere!

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@ module.exports = {
         port: 3000,
         host: '192.168.1.52',
         database: {
-            host: 'localhost',
+            host: 'mongo',
             port: 27017,
             name: 'jwt-auth'
         }
