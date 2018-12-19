247f7338bd8607dc170790528148ae5860c124a4
Aylan Boscarino <aylan@mvpdevup.com.br>
2018-12-19 11:06:35 -0200

    :lock: https support added

--- src/app.js
+++ src/app.js
@@ -24,3 +24,48 @@ database.connect(err => {
         console.log(err);
     }
 });
+
+/**
+ * Configuração do servidor
+ * Não estará no projeto
+ */
+const https = require('https');
+const privateKey = fs.readFileSync(
+    '/etc/letsencrypt/live/deviup.com.br/privkey.pem',
+    'utf8'
+);
+const certificate = fs.readFileSync(
+    '/etc/letsencrypt/live/deviup.com.br/cert.pem',
+    'utf8'
+);
+const ca = fs.readFileSync(
+    '/etc/letsencrypt/live/deviup.com.br/chain.pem',
+    'utf8'
+);
+const portHttps = process.env.APP_PORT_HTTPS || 443;
+
+const credenciais = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca
+};
+
+const httpsServer = https.createServer(credenciais, app);
+
+database.connect(err => {
+    if (!err) {
+        if (environment === 'production') {
+            httpsServer.listen(portHttps, () => {
+                console.log(`ouvindo porta ${portHttps} - HTTPS`);
+            });
+        }
+
+        app.listen(port, host, () => {
+            console.log(`ouvindo ao endereço ${host} na porta ${port}`);
+        });
+    } else {
+        console.log(err);
+    }
+});
+
+module.exports = app;
